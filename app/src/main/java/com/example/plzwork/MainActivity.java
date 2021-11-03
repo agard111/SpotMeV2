17dcb3c054a6a86945354459cdb38e7e754ca985
agard111 <anshulgarde3000@gmail.com>
2021-11-02 23:45:37 -0700

    initial push

---
+++ app/src/main/java/com/example/plzwork/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.plzwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    SignInButton btnSignIn;
+    private GoogleSignInClient mGoogleSignInClient;
+    public static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        requestGoogleSignIn();
+
+        btnSignIn.setOnClickListener(view -> {
+            signIn();
+        });
+
+    }
+
+    private void requestGoogleSignIn(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+}
