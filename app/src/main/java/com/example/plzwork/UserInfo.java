7ffbfab08384712b79a9b1d88302dd706667182a
agard111 <anshulgarde3000@gmail.com>
2021-11-09 19:30:10 -0800

    merged kai and anshuls code 11/9/2021

---
+++ app/src/main/java/com/example/plzwork/UserInfo.java
@@ -0,0 +1,25 @@
+package com.example.plzwork;
+
+public class UserInfo {
+    private String name,email;
+
+    public UserInfo() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/plzwork/UserProfileActivity.java
+++ app/src/main/java/com/example/plzwork/UserProfileActivity.java
@@ -1,19 +1,29 @@
 package com.example.plzwork;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -24,7 +34,8 @@ public class UserProfileActivity extends AppCompatActivity {
     Button btnSignOut;
     private FirebaseDatabase db = FirebaseDatabase.getInstance();//kai code
     private DatabaseReference root = db.getReference().child("Users");//kai code
-    //private DatabaseReference dataReference = db.getReference();//kai code
+    private DatabaseReference dataReference = db.getReference();//kai code
+    private ListView mListView; //kai code
 
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser user = mAuth.getCurrentUser();
@@ -39,7 +50,22 @@ public class UserProfileActivity extends AppCompatActivity {
         usermap.put("Name", user.getDisplayName());//kai code
         usermap.put("Email", user.getEmail());//kai code
 
-        root.child("" + 0).setValue(usermap);
+        root.child(user.getUid()).setValue(usermap);//kai code
+
+        dataReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                showData(snapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
 
 
         tvUserName = findViewById(R.id.username);
@@ -54,7 +80,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
 
         btnSignOut.setOnClickListener(view ->{
-            //mAuth.signOut();
+
             mAuth.signOut();
 
 
@@ -64,20 +90,25 @@ public class UserProfileActivity extends AppCompatActivity {
         });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
+
+    private void showData(DataSnapshot snapshot) {
+        int i = 0;
+        for(DataSnapshot data:snapshot.getChildren()) {
+            UserInfo user = new UserInfo();
+            user.setName(data.child("" + ++i).getValue(UserInfo.class).getName());
+            user.setEmail(data.child("" + ++i).getValue(UserInfo.class).getEmail());
+
+            Log.d(TAG, "Name: " + user.getName());
+            Log.d(TAG, "Email: " + user.getEmail());
+
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(user.getName());
+            arr.add(user.getEmail());
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, arr);
+            mListView.setAdapter(adapter);
+        }
+    }
+
 }
