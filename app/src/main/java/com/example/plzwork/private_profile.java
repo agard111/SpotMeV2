cf176a17d3ade4e74af5cf8176cb9d20d83646e8
anshulgarde3000@gmail.com <anshulgarde3000@gmail.com>
2021-11-28 12:23:21 -0800

    Added UI for private profiles

---
+++ app/src/main/java/com/example/plzwork/private_profile.java
@@ -0,0 +1,14 @@
+package com.example.plzwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class private_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_profile);
+    }
+}
