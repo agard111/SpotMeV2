d27c64b20ffacd3b7dea91be9363de42496991ba
tedd-E <62875128+tedd-E@users.noreply.github.com>
2021-11-30 17:12:57 -0800

    user feed displays first six profiles in database

--- app/src/main/java/com/example/plzwork/feed.java
+++ app/src/main/java/com/example/plzwork/feed.java
@@ -3,32 +3,22 @@ package com.example.plzwork;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.*;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.*;
 
 public class feed extends AppCompatActivity {
 
@@ -47,30 +37,76 @@ public class feed extends AppCompatActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         //TODO: make UI run in an infinite loop to display multiple user profiles w RecyclerView
+                        List<ImageView> profilePictures = new ArrayList<>();
+                        profilePic = findViewById(R.id.feedImg1); profilePictures.add(profilePic);
+                        profilePic = findViewById(R.id.feedImg2); profilePictures.add(profilePic);
+                        profilePic = findViewById(R.id.feedImg3); profilePictures.add(profilePic);
+                        profilePic = findViewById(R.id.feedImg4); profilePictures.add(profilePic);
+                        profilePic = findViewById(R.id.feedImg5); profilePictures.add(profilePic);
+                        profilePic = findViewById(R.id.feedImg6); profilePictures.add(profilePic);
+
+                        List<TextView> usernames = new ArrayList<>();
+                        retrieveUser = findViewById(R.id.user1); usernames.add(retrieveUser);
+                        retrieveUser = findViewById(R.id.user2); usernames.add(retrieveUser);
+                        retrieveUser = findViewById(R.id.user3); usernames.add(retrieveUser);
+                        retrieveUser = findViewById(R.id.user4); usernames.add(retrieveUser);
+                        retrieveUser = findViewById(R.id.user5); usernames.add(retrieveUser);
+                        retrieveUser = findViewById(R.id.user6); usernames.add(retrieveUser);
+
+                        List<TextView> fitnessLevels = new ArrayList<>();
+                        retrieveFitness = findViewById(R.id.fitness1); fitnessLevels.add(retrieveUser);
+                        retrieveFitness = findViewById(R.id.fitness2); fitnessLevels.add(retrieveUser);
+                        retrieveFitness = findViewById(R.id.fitness3); fitnessLevels.add(retrieveUser);
+                        retrieveFitness = findViewById(R.id.fitness4); fitnessLevels.add(retrieveUser);
+                        retrieveFitness = findViewById(R.id.fitness5); fitnessLevels.add(retrieveUser);
+                        retrieveFitness = findViewById(R.id.fitness6); fitnessLevels.add(retrieveUser);
+
+                        List<TextView> availabilities = new ArrayList<>();
+                        retrieveAvailable = findViewById(R.id.availability1); availabilities.add(retrieveAvailable);
+                        retrieveAvailable = findViewById(R.id.availability2); availabilities.add(retrieveAvailable);
+                        retrieveAvailable = findViewById(R.id.availability3); availabilities.add(retrieveAvailable);
+                        retrieveAvailable = findViewById(R.id.availability4); availabilities.add(retrieveAvailable);
+                        retrieveAvailable = findViewById(R.id.availability5); availabilities.add(retrieveAvailable);
+                        retrieveAvailable = findViewById(R.id.availability6); availabilities.add(retrieveAvailable);
+
+                        List<TextView> locations = new ArrayList<>();
+                        retrieveLocations = findViewById(R.id.location1); locations.add(retrieveLocations);
+                        retrieveLocations = findViewById(R.id.location2); locations.add(retrieveLocations);
+                        retrieveLocations = findViewById(R.id.location3); locations.add(retrieveLocations);
+                        retrieveLocations = findViewById(R.id.location4); locations.add(retrieveLocations);
+                        retrieveLocations = findViewById(R.id.location5); locations.add(retrieveLocations);
+                        retrieveLocations = findViewById(R.id.location6); locations.add(retrieveLocations);
+
+                        int index = 0;
                         for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                             String username = snapshot.child("Name").getValue(String.class);
-                            retrieveUser = findViewById(R.id.textView5);
+                            retrieveUser = usernames.get(index);
                             retrieveUser.setText("Username: " + username);
 
                             String url = snapshot.child("Image").getValue(String.class);
-                            profilePic = findViewById(R.id.feedImg1);
+
+                            profilePic = profilePictures.get(index);
                             Glide.with(context).load(url).into(profilePic);
 
 
                             String fitness = snapshot.child("Fitness Level").getValue(String.class);
-                            retrieveFitness = findViewById(R.id.textView7);
+                            retrieveFitness = fitnessLevels.get(index);
                             retrieveFitness.setText("Fitness Level: " + fitness);
 
                             String days = snapshot.child("Days Available").getValue(String.class); //POG IT WORKS
-                            retrieveAvailable = findViewById(R.id.textView8);
+                            retrieveAvailable = availabilities.get(index);
                             retrieveAvailable.setText("Days Available: " + days);
 
-                            
+
                             String location = snapshot.child("Location").getValue(String.class);
-                            retrieveLocations = findViewById(R.id.textView9);
+                            retrieveLocations = locations.get(index);
                             retrieveLocations.setText("Location: " + location);
+
+                            if(index < profilePictures.size() - 1)  //TODO: recyclerview :(
+                                index++;
                         }
                     }
+
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                     }
@@ -104,18 +140,18 @@ public class feed extends AppCompatActivity {
         user_database = FirebaseDatabase.getInstance();
 
         Username = user_database.getReference("Username");
-        retrieveUser = findViewById(R.id.textView5);
+        retrieveUser = findViewById(R.id.user1);
 
 
         Fitness_Level = user_database.getReference("Fitness Level");
-        retrieveFitness = findViewById(R.id.textView7);
+        retrieveFitness = findViewById(R.id.fitness1);
 
         Availability = user_database.getReference("Availability");
-        retrieveAvailable = findViewById(R.id.textView8);
+        retrieveAvailable = findViewById(R.id.availability1);
 
 
         Locations = user_database.getReference("Locations");
-        retrieveLocations = findViewById(R.id.textView9);
+        retrieveLocations = findViewById(R.id.location1);
 
 
     }
